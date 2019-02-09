d5b6e116ba82d1546fc916fecc0c6ad9ac7fd4f5
Mikael Ribeiro <mikael@ribeiro.se>
2019-02-10 00:56:38 +0100

    events

--- scripts/main.js
+++ scripts/main.js
@@ -16,9 +16,9 @@ var roomDiv = document.getElementById('room');
 var roomselect = document.getElementById('login-rooms-select');
 var usernameInput = document.getElementById('login-username-input');
 var signInButton = document.getElementById('login-button');
+var leaveRoomButton = document.getElementById('leave-rrom-button');
 
-
-
+/*
 function allowDrop(ev) {
     ev.preventDefault();
   }
@@ -33,42 +33,51 @@ function allowDrop(ev) {
     ev.target.appendChild(document.getElementById(data));
   }
 
-
+*/
   
-var bootstrapAlert = function () { }
-bootstrapAlert.info = function (message, type, header) {
-    $('#alert_placeholder').html('<div class="alert alert-' + type + '"><a href="#" class="close" data-dismiss="alert" aria-label="close">×</a><strong>' + header + '</strong> ' + message + '</div>');
+var showAlert = function () { }
+showAlert.info = function (message) {
+    
+    $("#alerttext").text(message);
+    $("#info-alert").show();
+
+    $("#info-alert").fadeTo(2000, 500).slideUp(500, function(){
+            $("#info-alert").slideUp(500);
+         });  
+
+    //$('#alert_placeholder').html('<div class="alert alert-' + type + '"><a href="#" class="close" data-dismiss="alert" aria-label="close">×</a><strong>' + header + '</strong> ' + message + '</div>');
 }
 
 
 //################ ROOM ###########################
 
 
-database.ref('rooms').on("child_added",function(snapshot){
-
-    //update list of users in a room.
-    var room = snapshot.val();
-
-});
-
 function loadUsersList(){
 
     if(currentRoom)
     {
-          database.ref('rooms/'+currentRoom+'/users').on("child_added", function(snapshot, prevChildKey) {
+        var usersRef = database.ref('rooms/'+currentRoom+'/users');
+        usersRef.on("child_added", function(snapshot, prevChildKey) {
             var newUser = snapshot.val();       
             console.log(newUser);   
             $("#userslist").append('<li class="list-group-item">'+ newUser.name +'</li>');
-          });
+
+            showAlert.info(newUser.name + " joined the fight!");
+        });
+
+
+        usersRef.onDisconnect().remove();
+
+
     }
 }
 
 
-function addUserToRoom(){
+function addUserToRoom(user){
 
-    database.ref('rooms/'+currentRoom+'/users/'+currentUser).update({"name":currentUser});
-
+    database.ref('rooms/'+currentRoom+'/users/'+currentUser).update({"name":user});
 }
+
 
 //################ ROOM END ###########################
 
@@ -76,7 +85,7 @@ function initGame(){
 
     loadUsersList();
 
-    addUserToRoom();
+    addUserToRoom(currentUser);
 
 }
 
@@ -95,47 +104,79 @@ function loadRoomDropdown(){
       });
 }
 
-     // Bind Sign in button.
-     signInButton.addEventListener('click', function() {
+// Bind Sign in button.
+signInButton.addEventListener('click', function() {
 
-        loginDiv.style.display = 'none'; 
-        roomDiv.style.display = 'block';
+    if (typeof currentRoom !== 'undefined')
+    {
+        if(usernameInput.value !== null)
+        {
+            loginDiv.style.display = 'none'; 
+            roomDiv.style.display = 'block';
+        
+            currentUser = usernameInput.value;
 
-        currentUser = usernameInput.value;
+            localStorage.setItem("user",currentUser);
+        
+            initGame();
+        }
+    }
 
-        initGame();
-    });
+
+});
 
 // #################### LOGIN END ###############################
 
-
-
-//Using set() overwrites data at the specified location, including any child nodes.
-//
-function writeUserData(userId, name) {
-    database.ref('users/' + userId).set({
-      username: name
-    });
-  }
 
  
 
 //$(function () {
 $(document).ready(function(){  
 
+    var room = localStorage.getItem("room");
+    var user = localStorage.getItem("user");
+
+    if(room !== null)
+    {
+        currentRoom = room;
+    }
+
+    if(user !== null)
+    {
+        currentUser = user;
+    }
+
+    if(room !== null && user != null)
+    {
+        loginDiv.style.display = 'none'; 
+        roomDiv.style.display = 'block';
+
+        initGame();
+        return;
+    }
+
     console.log("document ready");
+
     roomDiv.style.display = 'none'; 
 
     loadRoomDropdown();
   
 
     $("#login-rooms-select").change(function(){
+
         var selectedRoom = $(this).children("option:selected").val();
         currentRoom = selectedRoom;
+
+        localStorage.setItem("room",currentRoom);
+
         console.log("You have selected the room - " + selectedRoom);
+
     });
 
 
+    leaveRoomButton.addEventListener('click', function() {
+        
+    });
 
 
 });
