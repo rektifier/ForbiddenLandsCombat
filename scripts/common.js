49469c45b1b19b434453441bf3128f63773a6e63
Mikael Ribeiro <mikael@ribeiro.se>
2019-03-28 13:33:25 +0100

    generate random key

--- scripts/common.js
+++ scripts/common.js
@@ -74,6 +74,12 @@ var logger = function()
 
 var gamesToPlay = [{name:'Svärdets Sång',url:'g/forbiddenlands/index.html'},{name:'The End Of The World',url:'g/teotw/index.html'}];
 
-
+var generateId = function () {
+    // Math.random should be unique because of its seeding algorithm.
+    // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+    // after the decimal.
+    return '_' + Math.random().toString(36).substr(2, 9);
+  };
+  
 
 //logger.disableLogger();
